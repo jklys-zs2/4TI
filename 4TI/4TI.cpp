457449f671fb3312aca7ccfff6f12907c15026e9
Jakub Kłys <jklys@zs2pabianice.pl>
2026-09-18 15:34:27 +0200

    Rozbudowa funkcji i obsługa UTF-8 w 4TI.cpp
    Zmieniono strukturę programu, dodano obsługę kodowania UTF-8 na Windowsie oraz ustawienie lokalizacji na "Polish". Zmieniono nazwę zmiennej wiekUcznia na wiekUczniaLata, dodano zmienne: waga, wzrost, płeć, imię, pełnoletniość ucznia wraz z komentarzami. Ulepszono sprawdzanie pełnoletniości i komunikaty. Dodano modyfikację pierwszej litery imienia oraz wypisywanie trzech pierwszych liter w pętli. Usunięto stare przykłady wypisania tekstu i uproszczono warunek logiczny.

--- 4TI/4TI.cpp
+++ 4TI/4TI.cpp
@@ -2,21 +2,48 @@
 //
 
 /*
-* Komentarz w bloku.
+ Komentarz w bloku.
 */
 
 #include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 using namespace std;
 
-int main(){
-    int wiekUcznia = 15;
+int main()
+{
+	// Ustawienie kodowania konsoli na UTF-8, aby poprawnie wyświetlać polskie znaki
+#ifdef _WIN32
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+#endif
+	setlocale(LC_ALL, "Polish");
+	int wiekUczniaLata = 0; // nie stosuje nazwy typu int wiek = 15; bo nie wiadomo czy to wiek w latach czy w miesiącach
 	float wagaUczniaKg = 50.5; // nie stosuje nazwy typu float kg = 50.5; bo nie wiadomo czy to waga w kg czy funtach
-
-    if (true) {
-		cout << "Prawda" << endl;
-    }
-    cout << "Hello World!" << endl;
+	double wzrostUczniaM = 1.75; // nie stosuje nazwy typu double m = 1.75; bo nie wiadomo czy to w metrach czy w stopach
+	// zmienna double jest zmienną podwójnej precyzji, czyli może przechowywać liczby zmiennoprzecinkowe z większą dokładnością niż zmienna float
+	char plecUcznia = 'M'; // nie stosuje nazwy typu char p = 'M'; bo nie wiadomo czy to płeć czy coś innego
+	string imieUcznia = "Jan"; // nie stosuje nazwy typu string i = "Jan"; bo nie wiadomo czy to imię czy coś innego
+	bool czyUczenJestPelnoletni = false; // nie stosuje nazwy typu bool b = false; bo nie wiadomo czy to jest zmienna logiczna czy coś innego
+	cout << "Podaj wiek ucznia w latach: "; // << to operator przesunięcia bitowego w lewo, ale w tym przypadku jest używany do wyświetlania tekstu na ekranie
+	cin >> wiekUczniaLata; // >> to operator przesunięcia bitowego w prawo, ale w tym przypadku jest używany do wczytywania danych z klawiatury
+	if (wiekUczniaLata >= 18)czyUczenJestPelnoletni = true; // jeśli wiek ucznia jest większy lub równy 18 lat, to zmienna czyUczenJestPelnoletni przyjmuje wartość true
+	else czyUczenJestPelnoletni = false; // jeśli wiek ucznia jest mniejszy niż 18 lat, to zmienna czyUczenJestPelnoletni przyjmuje wartość false
+    if (czyUczenJestPelnoletni) 
+	{
+		cout << "Uczen jest pelnoletni.\n"; // \n to znak nowej linii, czyli przejście do nowego wiersza
+	}
+	else
+	{
+		cout << "Uczen nie jest pelnoletni." << endl; // endl to znak nowej linii, czyli przejście do nowego wiersza
+	}
+	// cout << "Imie ucznia: " << imieUcznia << endl;
+	imieUcznia[0] = 'K'; // zmiana pierwszej litery imienia ucznia na 'K'
+	for (int i = 0; i < 3; i++)cout << imieUcznia[i]; // pętla for, która wykonuje się 3 razy
+	cout << endl; // przejście do nowego wiersza
+    cout << "Koniec działania programu" << endl;
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
